8a51aa7529a000a93791944cd459fbe44d68e169
Gato Junior <dev@gatojunior.co>
2022-01-06 15:24:42 +0100

    finalisation de page panier

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from store.models import Product
+from carts.models import CartItem
+from carts.views import _cart_id
 from category.models import Category
 # Create your views here.
 def store(request, category_slug=None):
@@ -22,13 +24,14 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
-       # in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
 
 
     context = {
         'single_product': single_product,
+        'in_cart'       : in_cart,
        
     } 
     return render(request, 'store/product_detail.html',context)
