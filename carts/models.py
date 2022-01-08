f835c20a15711a9c056f8ebc62015c2f29879b2d
Gato Junior <dev@gatojunior.co>
2022-01-08 01:33:27 +0100

    variation des produit taille couleur

--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from store.models import Product
+from store.models import Product, Variation
 # Create your models here.
 class Cart(models.Model):
     cart_id = models.CharField(max_length=250, blank=True)
@@ -10,10 +10,13 @@ class Cart(models.Model):
 class CartItem(models.Model):
     
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variations = models.ManyToManyField(Variation, blank=True)
     cart    = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
+
     def sub_total(self):
         return self.product.price * self.quantity
-    def __str__(self):
+
+    def __unicode__(self):
         return self.product
--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from carts.models import Cart, CartItem
-from store.models import Product
+from store.models import Product, Variation
 from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 def _cart_id(request):
@@ -9,8 +9,18 @@ def _cart_id(request):
         cart = request.session.create()
     return cart
 def add_cart(request, product_id):
-    
     product = Product.objects.get(id=product_id) #get the product
+    if request.method == 'POST':
+        product_variation = []
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+            try:
+                variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+                product_variation.append(variation)
+            except:
+                pass
+    
    
     try:
         cart= Cart.objects.get(cart_id=_cart_id(request))
